c61d1f9aa7b8aa3911bf7ed947928aa03df4f3d1
Al-Ahmadgaid Asaad <alstated@gmail.com>
2016-01-14 00:01:53 +0800

    New Files

---
+++ R/alasaad.R
@@ -0,0 +1,11 @@
+#' Variational Bayesian Inference for ANN/HMM
+#' 
+#' @description
+#' A package supplementing the thesis authored
+#' 
+#' @author Al-Ahmadgaid B. Asaad
+#' @import Rcpp reshape2 lattice
+#' @docType package
+#' @useDynLib alasaad
+#' @name alasaad-package
+NULL
